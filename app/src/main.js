cb07d395fefb69734349b29b156ac3087b258511
Johny fabrin <johny@prof.unipar.br>
2018-11-13 18:06:50 -0300

    Aula 08/11

--- app/src/main.js
+++ app/src/main.js
@@ -2,14 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App'
 import router from './router'
 
 import 'vuetify/dist/vuetify.min.css'
 import 'babel-polyfill'
 
+axios.defaults.baseURL = 'http://localhost:3000/api/'
+
 Vue.config.productionTip = false
 Vue.use(Vuetify)
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
--- app/src/pages/Cliente.vue
+++ app/src/pages/Cliente.vue
@@ -4,10 +4,22 @@
     <v-content class="ma-3">
       <h1>Cliente</h1>
       <hr />
+      <v-layout wrap>
+        <v-flex xs12>
+          <v-text-field v-model="filtro" label="Filtro" />
+        </v-flex>
+      </v-layout>
       <v-data-table :headers="tblCliente.cabecalho" 
         :items="tblCliente.item" hide-actions
-        class="elevation-1">
-
+        class="elevation-1"
+        :search="filtro">
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.id }}</td>
+          <td>{{ props.item.nome }}</td>
+          <td>{{ props.item.telefone }}</td>
+          <td>{{ props.item.email }}</td>
+          <td>{{ props.item.situacao | situacao }}</td>
+        </template>
       </v-data-table>
     </v-content>
   </div>
@@ -16,11 +28,15 @@
 <script>
 import Cabecalho from '@/components/Cabecalho'
 export default {
+  created () {
+    this.buscarDados()
+  },
   components: {
     Cabecalho
   },
   data () {
     return {
+      filtro: '',
       tblCliente: {
         cabecalho: [
           {text: '#', value: 'id'},
@@ -31,6 +47,23 @@ export default {
         ],
         item: []
       }
+    }
+  },
+  filters: {
+    situacao: function (val) {
+      if (!val) return
+      if (val === 'A') return 'Ativo'
+      else return 'Inativo'
+    }
+  },
+  methods: {
+    buscarDados () {
+      this
+        .axios
+        .get('clientes')
+        .then((success) => {
+          this.tblCliente.item = success.data
+        })
     }
   }
 }
