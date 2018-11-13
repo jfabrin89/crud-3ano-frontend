026424d087590e25f04160a1e809ff8efe502862
Johny fabrin <johny@prof.unipar.br>
2018-11-13 19:40:56 -0300

    Parte 2 - Aula 13/11

--- app/src/components/ModalClienteCadastro.vue
+++ app/src/components/ModalClienteCadastro.vue
@@ -14,12 +14,20 @@
         </v-toolbar>
         <v-layout row wrap justify-center>
           <v-flex xs8>
+            <v-alert :type="mensagem.tipo" :value="true" v-if="mensagem.mostrar">
+              {{mensagem.texto}}
+            </v-alert>
             <v-form v-model="valid" ref="form" lazy-validation>
               <v-text-field v-model="cliente.id" label="ID" disabled />
               <v-text-field v-model="cliente.nome" label="Nome" />
               <v-text-field v-model="cliente.telefone" label="Telefone" />
               <v-text-field v-model="cliente.email" label="E-mail" />
               <v-select v-model="cliente.situacao" label="Situação" :items="cbb.situacao" />
+
+              <v-btn flat color="primary" @click="salvar">
+                <v-icon class="mr-2">save</v-icon>
+                Salvar
+              </v-btn>
             </v-form>
           </v-flex>
         </v-layout>
@@ -45,12 +53,36 @@ export default {
         email: '',
         situacao: 'A'
       },
+      mensagem: {
+        tipo: '',
+        mostrar: false,
+        texto: ''
+      },
       valid: true
     }
   },
   methods: {
     close (val) {
       this.$emit('cliente', val)
+    },
+    salvar () {
+    this
+      .axios
+      .post('clientes', this.cliente)
+      .then((success) => {
+        this.mensagem = {
+          mostrar: true,
+          texto: 'Salvo com sucesso',
+          tipo: 'success'
+        }
+      })
+      .catch((error) => {
+        this.mensagem = {
+          mostrar: true,
+          texto: 'Erro ao salvar ' + error,
+          tipo: 'error'
+        }
+      })
     }
   },
   props: [
