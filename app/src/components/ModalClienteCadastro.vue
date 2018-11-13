f5caaf85a73b91464dc0ce646666f02bcc513faa
Johny fabrin <johny@prof.unipar.br>
2018-11-13 19:01:17 -0300

    Parte 1 - Aula 13/11

---
+++ app/src/components/ModalClienteCadastro.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="modalClienteCadastro">
+    <v-dialog 
+      v-model="modal"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition">
+      <v-card>
+        <v-toolbar dark>
+          <v-btn icon dark @click.native="close(false)">
+            <v-icon>close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Cadastro de Cliente</v-toolbar-title>
+        </v-toolbar>
+        <v-layout row wrap justify-center>
+          <v-flex xs8>
+            <v-form v-model="valid" ref="form" lazy-validation>
+              <v-text-field v-model="cliente.id" label="ID" disabled />
+              <v-text-field v-model="cliente.nome" label="Nome" />
+              <v-text-field v-model="cliente.telefone" label="Telefone" />
+              <v-text-field v-model="cliente.email" label="E-mail" />
+              <v-select v-model="cliente.situacao" label="Situação" :items="cbb.situacao" />
+            </v-form>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cbb: {
+        situacao: [
+          {text: 'Ativo', value: 'A'},
+          {text: 'Inativo', value: 'I'}
+        ]
+      },
+      cliente: {
+        id: '',
+        nome: '',
+        telefone: '',
+        email: '',
+        situacao: 'A'
+      },
+      valid: true
+    }
+  },
+  methods: {
+    close (val) {
+      this.$emit('cliente', val)
+    }
+  },
+  props: [
+    'modal'
+  ]
+}
+</script>
+
+<style>
+
+</style>
--- app/src/pages/Cliente.vue
+++ app/src/pages/Cliente.vue
@@ -20,19 +20,41 @@
           <td>{{ props.item.email }}</td>
           <td>{{ props.item.situacao | situacao }}</td>
         </template>
+        <template slot="no-data">
+          <v-btn flat color="primary" @click="buscarDados">
+            <v-icon class="mr-2">autorenew</v-icon>
+            Recarregar
+          </v-btn>
+        </template>
       </v-data-table>
+      <div style="position: relative">
+        <v-btn 
+          absolute
+          small
+          dark
+          fab
+          top
+          right
+          @click="modal.cliente = !modal.cliente">
+          <v-icon>add</v-icon>
+        </v-btn>
+      </div>
     </v-content>
+
+    <ModalClienteCadastro :modal="modal.cliente" v-on:cliente="closeCliente" />
   </div>
 </template>
 
 <script>
 import Cabecalho from '@/components/Cabecalho'
+import ModalClienteCadastro from '@/components/ModalClienteCadastro'
 export default {
   created () {
     this.buscarDados()
   },
   components: {
-    Cabecalho
+    Cabecalho,
+    ModalClienteCadastro
   },
   data () {
     return {
@@ -46,6 +68,9 @@ export default {
           {text: 'Situação', value: 'situacao'}
         ],
         item: []
+      },
+      modal: {
+        cliente: false
       }
     }
   },
@@ -64,6 +89,9 @@ export default {
         .then((success) => {
           this.tblCliente.item = success.data
         })
+    },
+    closeCliente (val) {
+      this.modal.cliente = val
     }
   }
 }
